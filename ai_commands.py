98ea268009ad4c2138152182072a383e35a8cc47
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2024-02-17 21:01:30 +0000

    TESTFIX: fixed up until sending / retrieving an image response, but the edited image is not good

--- ai_commands.py
+++ ai_commands.py
@@ -126,7 +126,7 @@ def edit_image(message, org_image_file_byte_array):
             response_format='url'
             )
         
-        print(ImagesResponse)
+        # print(ImagesResponse)
         response = requests.get(ImagesResponse.data[0].url)
         return response.content
     
