3b854846ee35dc2d611258a16d56c5e45a235089
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2024-06-25 13:41:27 +0100

    FEATURE_UPDATE: MarkdownV2 introduced to all chat features

--- app.py
+++ app.py
@@ -317,9 +317,9 @@ def is_in_reply(func):
     return wrapper
 
 
-def escape_markdown(text):
-    escape_chars = '_*[]()~`>#+-=|{}.!'
-    return ''.join(f'\\{char}' if char in escape_chars else char for char in text)
+# def escape_markdown(text):
+#     escape_chars = '_*[]()~`>#+-=|{}.!'
+#     return ''.join(f'\\{char}' if char in escape_chars else char for char in text)
 
 
 
@@ -388,7 +388,7 @@ def handle_chat(message):
             print("No context was set for user")
 
         response_text = ai_commands.chat_completion(message, context, chat_history = chat_history, openai_api_key=api_keys[0], model=chat_config['language_model'], temperature=chat_config['lm_temp'])
-        bot.reply_to(message, text=escape_markdown(response_text), parse_mode='Markdown')
+        bot.reply_to(message, text=response_text, parse_mode='MarkdownV2')
         logger.info(helper_functions.construct_logs(message, f"Success: response generated and sent."))
 
         # if the user is a premium user, and is the user wanting to save chat history for this chat group?
@@ -427,7 +427,7 @@ def handle_translate_1(message):
 
         if api_keys:
             response_text = ai_commands.translate(message, openai_api_key=api_keys[0], target_language=chat_config['t1'], model=chat_config['language_model'])
-            bot.reply_to(message, text=escape_markdown(response_text), parse_mode='Markdown')
+            bot.reply_to(message, text=response_text, parse_mode='MarkdownV2')
             logger.info(helper_functions.construct_logs(message, "Success"))
     except Exception as e:
         bot.reply_to(message, "/translate command request could not be completed, please contact admin.")
@@ -449,7 +449,7 @@ def handle_translate_2(message):
 
         if api_keys:
             response_text = ai_commands.translate(message, openai_api_key=api_keys[0], target_language=chat_config['t2'], model=chat_config['language_model'])
-            bot.reply_to(message, text=escape_markdown(response_text), parse_mode='Markdown')
+            bot.reply_to(message, text=response_text, parse_mode='MarkdownV2')
             logger.info(helper_functions.construct_logs(message, "Success"))
         
     except Exception as e:
@@ -474,7 +474,7 @@ def handle_translate_3(message):
 
         if api_keys:
             response_text = ai_commands.translate(message, openai_api_key=api_keys[0], target_language=chat_config['t3'], model=chat_config['language_model'])
-            bot.reply_to(message, text=escape_markdown(response_text), parse_mode='Markdown')
+            bot.reply_to(message, text=response_text, parse_mode='MarkdownV2')
             logger.info(helper_functions.construct_logs(message, "Success"))
     except Exception as e:
         bot.reply_to(message, "/translate command request could not be completed, please contact admin.")
@@ -599,7 +599,7 @@ def handle_stc(message):
                     # use the stt text response to call the chat and send the response
                     context=''
                     response_text = ai_commands.chat_completion(message, context, openai_api_key=api_keys[0], model=chat_config['language_model'], temperature=chat_config['lm_temp'])
-                    bot.reply_to(message, text=escape_markdown(response_text), parse_mode='Markdown')
+                    bot.reply_to(message, text=response_text, parse_mode='MarkdownV2')
                     logger.info(helper_functions.construct_logs(message, f"Success: query response generated and sent."))
                 else:
                     bot.reply_to(message, "Could not convert speech to text")
