5aebeb0345f9cbc53f0b8358dce0a9bc6578d830
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2024-07-24 10:34:10 +0100

    FEATURE: data type assertion for user ids and keys stored in system config

--- app.py
+++ app.py
@@ -437,23 +437,24 @@ def check_and_get_valid_apikeys(message, user_cfg, chat_cfg):
     # check for API Keys
     system_config = get_or_create_chat_config(OWNER_USER_ID, 'owner')
     api_keys = config_db_helper.get_apikey_list(user_cfg, chat_cfg)
+    user_id = str(message.from_user.id)
 
     # if the first API key returned is a free credit (meaning the user did not set any of their own keys)
     if api_keys[0] == OPENAI_FREE_KEY:
 
         # check whether the user is inside the system_config for a free trial credit, add a fallback value if it is not
-        if message.from_user.id not in system_config['user_credit_dict']:
+        if user_id not in system_config['user_credit_dict']:
             print("user is not in the config!")
-            system_config['user_credit_dict'][message.from_user.id] = 5
+            system_config['user_credit_dict'][user_id] = 5
 
         # now check how much credit the user has, if its 0, it is returned out and the function is NOT called
-        if system_config['user_credit_dict'][message.from_user.id] < 1:
+        if system_config['user_credit_dict'][user_id] < 1:
             bot.reply_to(message, "OpenAI API could not be called as there is no API Key entered and the user has run out of free credits.")
             config_db_helper.set_new_config(OWNER_USER_ID, 'owner', system_config)
             return None
         
         # use the system config
-        system_config['user_credit_dict'][message.from_user.id] -= 1
+        system_config['user_credit_dict'][user_id] -= 1
         bot.reply_to(message, f"Using free trial credits, remaining: {system_config['user_credit_dict'][message.from_user.id]}")
     
     # if it is not, then just simply return the API keys
