0b1a93fa74c4df98bffff495e765cec923db7336
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2024-08-15 12:35:30 +0100

    FEATURE_UPGRADE: unban feature now also supports user id provision

--- app.py
+++ app.py
@@ -2415,6 +2415,39 @@ def ban_user(message):
 
 
 # unban user
+        
+@bot.message_handler(commands=['unban'])
+@is_bot_active
+@is_admin
+@is_in_reply
+def unban_user(message):
+    """
+    
+    """
+    try:
+        system_config = get_or_create_chat_config(OWNER_USER_ID, 'owner')
+        if message.reply_to_message:
+            user_id_banned = message.reply_to_message.from_user.id
+        elif helper_functions.extract_body(message) != "":
+            user_id_banned = helper_functions.extract_body(message)
+        else:
+            bot.reply_to(message, f"Invalid, either reply to a user's message OR provide their user ID.")
+            return
+        
+        if user_id_banned not in system_config['banned_users']:
+            system_config['banned_users'].remove(user_id_banned)
+            config_db_helper.set_new_config(OWNER_USER_ID, 'owner', system_config)
+            bot.reply_to(message, f"User has been successfully unbanned.")
+        else:
+            bot.reply_to(message, f"User is not in bannedl ist")
+
+    except Exception as e:
+        bot.reply_to(message, "Failed to complete command, please see logs")
+        logger.error(helper_functions.construct_logs(message, f"Error: {str(e)}"))
+
+
+
+
 @bot.message_handler(commands=['unban'])
 @is_bot_active
 @is_admin
